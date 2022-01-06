d9c0cf1f592f8e221b779fd8d1e3818b7e1a41eb
arushsingh <arush.singh@nagarro.com>
2022-01-06 13:41:58 +0530

    Todo App

---
+++ src/TodoList.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import Button from '@mui/material/Button';
+import AddIcon from '@mui/icons-material/Add';
+import ListCom from "./ListCom"
+
+const TodoList = () =>{
+  const [item, setItem] = useState("ok");
+  const [newItem, setNewItem] = useState([])
+
+  const itemEvent = (event) =>{
+    setItem(event.target.value)
+  }
+
+  const listOfItem = () =>{
+    setNewItem((prevValue) => {
+      return [...prevValue, item]
+    })
+    setItem("")
+  }
+
+  return(
+    <>
+      <div className="main_div">
+        <div className="center_div">
+          <br />
+          <h1>TodoList</h1>
+          <br />
+          <input type="text" placeholder="Add an item" onChange={itemEvent}/>
+          <Button className="newBtn" onClick={listOfItem}>
+            <AddIcon />
+          </Button>
+
+          <br />
+          <ol>
+            {newItem.map((val, index) => {
+              return <ListCom key={index} text={val} />
+            })}
+          </ol>
+          <br />
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default TodoList;
